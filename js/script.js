e4c04a4093db68bf598baf381bda6922c57268bf
Feross Aboukhadijeh <feross@gmail.com>
2011-11-24 19:15:46 -0800

    misc changes

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ soundManager.onload = function() {
 var App = Base.extend({
     cards: [],
     headerAtTop: false, // Starts in vertical center and gets moved up
-    
+
     onLoad: function() {
         var that = this;
-        
+
         $('#cards').isotope({
             animationEngine: 'best-available',
             itemSelector: 'li',
@@ -28,82 +28,82 @@ var App = Base.extend({
             // sortBy: 'lastIsFirst',
             // sortAscending: false
         });
-        
+
         $('#input').keyup(function(e) {
             if (e.keyCode == 13) {
                 that.submit.call(that);
             }
         });
-        
+
         $('button').click(function(e) {
             e.preventDefault();
             that.submit.call(that);
         });
-        
+
         $('#header small a').click(function(e) {
             e.preventDefault();
             var text = $(e.target).text();
             $('#input').attr('value', text);
             $('button').click();
         });
-        
+
         $('#cards li').live('click', function(e) {
             e.preventDefault();
             var code = $(e.target).attr('class').split(' ')[0];
             var word = $(e.target).find('.word').text();
             tts(word, code, $.noop);
         });
-        
+
         $('#input').focus();
-            
+
     },
-    
+
     submit: function() {
         var that = this,
             fromWord = $.trim($('#input').val()),
             ajaxReqs = []; // array of all the translation ajax requests
-            
+
         if (!fromWord) {
             tts('Type something first, dude.', 'en', $.noop);
             return;
         }
-        
+
         if (!App.headerAtTop) {
             $('#header').addClass('top');
-            App.headerAtTop = true; 
+            App.headerAtTop = true;
         }
-        
+
         this.cards = []; // reset cards
-        
+
 	    $.each(App.LANGS, function(i, lang) {
             if (lang.tts != 'yes') {
                 return;
             }
-	        
+
 	        var d = $.ajax({
 	           dataType: 'jsonp',
-	           
+
 	           url: App.TRANSLATE_URL_PREFIX+'&source='+encodeURIComponent('en')+
 	                '&target='+encodeURIComponent(lang.code)+
 	                '&q='+encodeURIComponent(fromWord),
-	                
+
 	           success: function(trans) {
 	               var toWord = trans && trans.data &&
 	                            trans.data.translations &&
 	                            trans.data.translations[0] &&
 	                            trans.data.translations[0].translatedText;
-	               
+
 	               if (toWord) {
 	                   var card = new Card(lang.code, lang.label, toWord);
 	                   that.cards.push(card);
 	               }
-	               
+
 	           }
-	           
+
 	        });
 	        ajaxReqs.push(d);
 	    });
-	    
+
         $.when.apply(undefined, ajaxReqs)
             .done(function() {
                 that.addEnglishCard.call(that, fromWord);
@@ -113,30 +113,30 @@ var App = Base.extend({
                 alert('One or more words failed to translate.');
             });
     },
-    
+
     // TODO: Generalize this to any fromLanguage.
     addEnglishCard: function(word) {
         this.cards.push(new Card('en', 'English', word));
     },
-    
+
     loadCards: function() {
-        
+
         // Sort cards
         this.cards = _.sortBy(this.cards, function(card) {
             return card.label;
         });
-        
+
         // Hide existing cards.
         $('#cards').isotope({ filter: ':not(li)'});
-        
+
         window.setTimeout(function() {
             // Remove hidden cards and restore original filter setting.
             $('#cards')
                 .isotope('remove', $('#cards li'))
                 .isotope({filter: ''});
-            
+
             Card.showAll(0);
-            
+
         }, 800);
 
     }
@@ -145,7 +145,7 @@ var App = Base.extend({
     LANGS: [
     {code: 'af', label: 'Afrikaans', tts: 'bad'},
     {code: 'sq', label: 'Albanian', tts: 'bad'},
-    {code: 'ar', label: 'Arabic', tts: 'yes'},
+    {code: 'ar', label: 'Arabic', tts: 'bad'},
     // {code: 'hy', label: 'Armenian'},
     // {code: 'az', label: 'Azerbaijani'},
     // {code: 'eu', label: 'Basque'},
@@ -165,7 +165,7 @@ var App = Base.extend({
     {code: 'gl', label: 'Galician', tts: 'no'},
     // {code: 'ka', label: 'Georgian'},
     {code: 'de', label: 'German', tts: 'yes'},
-    {code: 'el', label: 'Greek', tts: 'yes'},
+    {code: 'el', label: 'Greek', tts: 'bad'},
     {code: 'ht', label: 'Haitian Creole', tts: 'low'},
     {code: 'iw', label: 'Hebrew', tts: 'no'},
     {code: 'hi', label: 'Hindi', tts: 'low'},
@@ -174,7 +174,7 @@ var App = Base.extend({
     {code: 'id', label: 'Indonesian', tts: 'bad'},
     {code: 'ga', label: 'Irish', tts: 'no'},
     {code: 'it', label: 'Italian', tts: 'yes'},
-    {code: 'ja', label: 'Japanese', tts: 'yes'},
+    {code: 'ja', label: 'Japanese', tts: 'bad'},
     {code: 'ko', label: 'Korean', tts: 'yes'},
     // {code: 'la', label: 'Latin'},
     {code: 'lv', label: 'Latvian', tts: 'bad'},
@@ -187,7 +187,7 @@ var App = Base.extend({
     {code: 'pl', label: 'Polish', tts: 'yes'},
     {code: 'pt', label: 'Portuguese', tts: 'yes'},
     {code: 'ro', label: 'Romanian', tts: 'bad'},
-    {code: 'ru', label: 'Russian', tts: 'yes'},
+    {code: 'ru', label: 'Russian', tts: 'bad'},
     {code: 'sr', label: 'Serbian', tts: 'bad'},
     {code: 'sk', label: 'Slovak', tts: 'bad'},
     {code: 'sl', label: 'Slovenian', tts: 'no'},
@@ -202,9 +202,9 @@ var App = Base.extend({
     {code: 'cy', label: 'Welsh', tts: 'bad'},
     {code: 'yi', label: 'Yiddish', tts: 'no'}
     ],
-    
-    TRANSLATE_URL_PREFIX: 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCHW3quKpNV0gc9WNElG61xJv8AS5Ee3Rk'
-    
+
+    TRANSLATE_URL_PREFIX: 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCXZPVIG5OpDxBP_v9aJvj3bx2q-9Vo-zE'
+
 });
 
 var app = new App();
@@ -219,12 +219,12 @@ var Card = Base.extend({
         this.label = label;
         this.word = word;
     },
-    
+
     // Uses continuation-passing.
     say: function(c) {
         tts(this.word, this.code, c);
     }
-    
+
 },
 {
     // Uses continuation-passing style.
@@ -232,12 +232,12 @@ var Card = Base.extend({
         if (i >= app.cards.length) {
             return;
         }
-                
+
         var card = app.cards[i];
-        
+
         var $card = $('<li class="'+card.code+'"><span class="num">'+i+'</span><span class="label">'+card.label+'</span> <span class="word">'+card.word+'</span></li>');
         $('#cards').isotope('insert', $card);
-        
+
         var inputText = $('#input').val();
         card.say(function() {
             // If value in #input changed, then don't continue.
@@ -255,9 +255,9 @@ function tts(q, tl, c) {
     if (!soundManagerLoaded) {
         return;
     }
-    
+
     var hash = hex_sha1(q+'####'+tl);
-    
+
     soundManager
         .createSound({
             id: hash,
